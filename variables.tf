2816da7aa8f12091970379055ed3db2d203b327b
srikanth1798 <167841337+srikanth1798@users.noreply.github.com>
2025-07-01 20:04:44 +0530

    made chnages

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_vpc_cidr_block" {
+  description = "cidr block for vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+
+}
+
+variable "aws_subnet_cidr_block" {
+  type    = string
+  default = "10.0.1.0/24"
+
+}

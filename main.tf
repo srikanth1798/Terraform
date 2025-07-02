3a6dd16afbc069e523dfb1b893d81c6c8e6e3ac3
srikanth1798 <167841337+srikanth1798@users.noreply.github.com>
2025-07-02 20:07:11 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 resource "aws_vpc" "name" {
   cidr_block = var.aws_vpc_cidr_block
   tags = {
-    Name = "my-vpc"
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "name" {
+  vpc_id = var.aws.vpc.id
+  cidr_block =  var.aws_subnet_cidr_block
+  tags = {
+    Name = "terraform-subnet-01"
   }
 }
--- variables.tf
+++ variables.tf
@@ -4,9 +4,8 @@ variable "aws_vpc_cidr_block" {
   default     = "10.0.0.0/16"
 
 }
-
 variable "aws_subnet_cidr_block" {
   type    = string
-  default = "10.0.1.0/24"
+  default = "10.0.0.16/28"
 
 }
